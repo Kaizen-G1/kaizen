a1cc3dbcc5c6bfb727263c3b23e5148b1e3903a9
Rajan Gurung <bardangrg0@gmail.com>
2025-01-20 17:43:51 -0500

    banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,71 +1,59 @@
 import React from "react";
-import { StyleSheet, View, Platform } from "react-native";
-import { Card, Text } from "react-native-paper";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import { Text } from "react-native-paper";
 
 interface BannerProps {
   imageUrl: string; // Prop for the image URL
+  title: string;
+  subtitle: string;
+  caption: string;
 }
 
-const Banner: React.FC<BannerProps> = ({ imageUrl }) => {
+const Banner: React.FC<BannerProps> = ({
+  imageUrl,
+  title,
+  subtitle,
+  caption,
+}) => {
   return (
-    <Card style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Card.Cover
-          source={{ uri: imageUrl }} // Use the imageUrl prop
-          style={styles.imageBackground}
-        />
+    <View style={styles.container}>
+      <ImageBackground
+        source={{ uri: imageUrl }} // Use the imageUrl prop
+        style={styles.imageBackground}
+        resizeMode="cover" // Ensure the image covers the container
+      >
         <View style={styles.textContainer}>
-          <Text variant="displayMedium" style={styles.title}>
-            Big Sale
-          </Text>
-          <Text variant="titleLarge" style={styles.subtitle}>
-            Up to 50%
-          </Text>
-          <Text variant="bodyLarge" style={styles.caption}>
-            Happening Now
-          </Text>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.subtitle}>{subtitle}</Text>
+          <Text style={styles.caption}>{caption}</Text>
         </View>
-      </View>
-    </Card>
+      </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    margin: 16,
+    marginHorizontal: 20,
     borderRadius: 16,
-    // overflow: "hidden", // Keep the image and text within the rounded corners
-    ...Platform.select({
-      ios: {
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-      },
-      android: {
-        elevation: 5,
-      },
-    }),
-  },
-  imageContainer: {
-    position: "relative", // Make the image container relative
-    backgroundColor: "red",
+    overflow: "hidden",
   },
   imageBackground: {
     height: 200,
-    borderRadius: 16, // Apply borderRadius directly to the image for consistency
+    width: "100%",
+    borderRadius: 16,
   },
   textContainer: {
-    position: "absolute", // Position the text absolutely on top of the image
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
     justifyContent: "center",
-    alignItems: "flex-start", // Align to the left side
+    alignItems: "flex-start",
     paddingLeft: 16,
     paddingRight: 16,
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Optional: add background overlay for readability
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   title: {
     fontWeight: "bold",
