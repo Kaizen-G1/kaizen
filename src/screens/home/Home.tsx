67e21f15f3e7854d172771b040dd3c122cd3519a
Jonathan Reátegui <j.reategui2012@gmail.com>
2025-01-21 17:46:11 -0500

    Adding ImageSlider component

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,19 +1,29 @@
 // src/screens/HomeScreen.tsx
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Dimensions } from "react-native";
 import { Text, Button, Appbar, PaperProvider } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 // Components
 import FlashSaleScreen from "../../components/flash-sale/FlashSale";
+import ImageSlider from "tenzai-components/components/ImageSlider/ImageSlider";
 import CategoryList from "../../components/CategoryList";
 import TopProducts from "../../components/TopProducts"
 
 // Mock Data
+import { BANNERS_MOCK } from "../../mock/banners.mock";
 import { CATEGORIES_LIST_MOCK } from "../../mock/categories-list.mock";
 import { TOP_PRODUCTS_MOCK } from "../../mock/top-products.mock";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const MARGIN_HORIZONTAL = 14;
 
 const HomeScreen = () => {
+
+  const sliderWidth = Dimensions.get('window').width - MARGIN_HORIZONTAL * 2;
+
+  const handleImagePress = (id: string) => {
+    console.log(`Selected image ID: ${id}`);
+  };
 
   const handleSelectCategory = (id: string) => {
     console.log(`Selected category ID: ${id}`);
@@ -31,9 +41,8 @@ const HomeScreen = () => {
     <>
       <SafeAreaProvider>
         <PaperProvider>
-          <SafeAreaView style={styles.container}>
-
-            <ScrollView contentContainerStyle={styles.container}>
+          <SafeAreaView style={styles.safeArea}>
+            <ScrollView>
               {/* <View style={styles.container}>
                 <Appbar.Header>
                   <Appbar.Content title="Home" />
@@ -45,7 +54,14 @@ const HomeScreen = () => {
                   </Button>
                 </View>
               </View> */}
-
+              <View style={styles.sliderContainer}>
+                <ImageSlider
+                  data={BANNERS_MOCK}
+                  handleImagePress={handleImagePress}
+                  sliderWidth={sliderWidth}
+                />
+              </View>
+              
               <CategoryList
                 title="Categories"
                 categories={CATEGORIES_LIST_MOCK}
@@ -67,12 +83,17 @@ const HomeScreen = () => {
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: 25,
+  },
   appbar: {
     height: 48,
     backgroundColor: "red",
   },
-  container: {
-    flex: 1,
+  sliderContainer: {
+    marginHorizontal: MARGIN_HORIZONTAL,
+    marginBottom: 15,
   },
   content: {
     flex: 1,
