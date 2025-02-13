f34e9229196d3acbb414cca589028da695da6754
Jonathan Reátegui <j.reategui2012@gmail.com>
2025-02-13 10:22:32 -0500

    Depliyinh iOS and Android in local

--- .conflict-side-0/src/screens/list/tabs/one.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function OneScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>One Screen</Text>
-    </View>
-  );
-}
--- .conflict-side-1/src/screens/favourites/Favourites.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function FavouriteScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Favourite Screen</Text>
-    </View>
-  );
-}
