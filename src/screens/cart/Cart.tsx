816d8f3a46874783505525a59f43f893fd5cbf2a
Rajan Gurung <bardangrg0@gmail.com>
2025-03-06 18:52:29 -0500

    updated cart

--- src/screens/cart/Cart.tsx
+++ src/screens/cart/Cart.tsx
@@ -1,21 +1,8 @@
-import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-  StyleSheet,
-  FlatList,
-} from "react-native";
-import { IconButton, Card, PaperProvider } from "react-native-paper";
-import CustomIcon from "tenzai-components/components/CustomIcon/CustomIcon";
-import Counter from "../../components/counter/Counter";
-import CustomButton from "tenzai-components/components/CustomButton/CustomButton";
+import React, { useEffect } from "react";
+import { View, Text, ScrollView, FlatList } from "react-native";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../../RootNavigator";
-import { useIsFocused, useNavigation } from "@react-navigation/native";
-import CartItem from "../../components/CartItem";
 import { useAppDispatch, useAppSelector } from "../../services/constants";
 import {
   CartPayload,
@@ -23,32 +10,29 @@ import {
   getCartThunk,
   updateSubTotal,
 } from "./slice/CartSlice";
+import CartItem from "../../components/CartItem";
+import CustomButton from "tenzai-components/components/CustomButton/CustomButton";
 
 type CartItemProps = StackNavigationProp<RootStackParamList, "Home">;
 
 export default function CartScreen() {
   const navigation = useNavigation<CartItemProps>();
-
   const dispatch = useAppDispatch();
-
-  const { loading, error, success, response } = useAppSelector(
-    (state) => state.cart.cartList
-  );
-
-  const subTotal = useAppSelector((state) => state.cart.subTotal);
-
   const isFocused = useIsFocused();
 
-  useEffect(() => {
-    dispatch(getCartThunk());
-  }, [dispatch, isFocused]);
-
+  const { response } = useAppSelector((state) => state.cart.cartList);
+  const subTotal = useAppSelector((state) => state.cart.subTotal);
   const cartItems = response?.data.cart || [];
 
-  const handleDeleteFromCart = async (cart: CartPayload) => {
-    await dispatch(deleteFromCartThunk(cart)).then(() => {
+  useEffect(() => {
+    if (isFocused) {
       dispatch(getCartThunk());
-    });
+    }
+  }, [isFocused]);
+
+  const handleDeleteFromCart = async (cart: CartPayload) => {
+    await dispatch(deleteFromCartThunk(cart));
+    dispatch(getCartThunk());
   };
 
   return (
@@ -58,7 +42,6 @@ export default function CartScreen() {
           style={{
             flexDirection: "row",
             alignItems: "center",
-            justifyContent: "flex-start",
             gap: 5,
             paddingHorizontal: 18,
           }}
@@ -108,7 +91,6 @@ export default function CartScreen() {
         />
       </ScrollView>
 
-      {/* Total & Checkout */}
       <View
         style={{
           flexDirection: "row",
@@ -118,132 +100,17 @@ export default function CartScreen() {
           backgroundColor: "#F8F8F8",
         }}
       >
-        <View>
-          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-            <Text style={{ fontSize: 18, fontWeight: "heavy" }}>Total: </Text>$
-            {subTotal}
-          </Text>
-        </View>
+        <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+          <Text style={{ fontSize: 18, fontWeight: "heavy" }}>Total: </Text>$
+          {subTotal.toFixed(2)}
+        </Text>
         <CustomButton
           label="Checkout"
           onPress={() =>
-            navigation.navigate("Payment", {
-              cart: cartItems,
-              subTotal: subTotal,
-            })
+            navigation.navigate("Payment", { cart: cartItems, subTotal })
           }
         />
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 20,
-    marginBottom: 15,
-    backgroundColor: "#fff",
-  },
-  listcontainer: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-evenly",
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  editButton: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  productName: {
-    fontSize: 16,
-    marginVertical: 5,
-  },
-  timerText: {
-    backgroundColor: "#753742",
-    borderRadius: 7,
-    color: "#FFFFFF",
-    padding: 5,
-    fontFamily: "Raleway-Regular",
-    marginHorizontal: 2,
-  },
-  cartContainer: {
-    padding: 10,
-  },
-  productCard: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 2,
-  },
-  card: {
-    borderRadius: 10,
-    overflow: "hidden",
-  },
-  imageContainer: {
-    position: "relative",
-  },
-  productImage: {
-    width: 120,
-    height: 120,
-    borderRadius: 8,
-  },
-  deleteButton: {
-    position: "absolute",
-    bottom: 5,
-    left: 5,
-    backgroundColor: "white",
-  },
-  itemsFooterContainer: {
-    marginLeft: 15,
-    flex: 1,
-    justifyContent: "space-between",
-  },
-  infoContainer: {
-    flexDirection: "column",
-  },
-  priceAndCounterContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-  },
-  productDescription: {
-    fontSize: 14,
-    color: "#333",
-    marginBottom: 4,
-  },
-  productDetails: {
-    fontSize: 14,
-    color: "#666",
-    marginBottom: 4,
-  },
-  productPrice: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  totalContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 16,
-  },
-  cartCount: {
-    backgroundColor: "#6B3A2A",
-    borderRadius: 50,
-    paddingHorizontal: 10,
-    paddingVertical: 4,
-  },
-});
